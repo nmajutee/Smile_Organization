00a7a8cfdb2ca125b19324a7cb5107e325cd96a2
nmaju <nmajutee@gmail.com>
2025-10-16 11:21:37 +0100

    feat(calendar): add Event model linked to Project

--- apps/calendar_app/models.py
+++ apps/calendar_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+from apps.projects.models import Project
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='events', null=True, blank=True)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    location = models.CharField(max_length=200, blank=True)
+    description = models.TextField(blank=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['start']
+
+    def __str__(self):
+        return self.title
