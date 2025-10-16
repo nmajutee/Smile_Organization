0681fea80e886949dc1179b0fed10c147fbe3bbc
nmaju <nmajutee@gmail.com>
2025-10-16 11:25:50 +0100

    feat(core): add about and contact endpoints

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def about(request):
+    return Response({
+        "name": "Smile Organization",
+        "mission": "Connect volunteers and donors to projects that create impact."
+    })
+
+@api_view(['POST'])
+def contact(request):
+    data = request.data
+    # store or send email - placeholder
+    return Response({"message": "Thanks, we received your message", "data": data})
