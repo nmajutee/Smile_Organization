5aa9cdb6bf2a4df12c0015b57c158ed8ac8fefd3
nmaju <nmajutee@gmail.com>
2025-10-16 11:18:48 +0100

    chore(blog): register blog models in admin

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post, Category, Tag
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','author','status','published_at','created_at']
+    list_filter = ['status','published_at','created_at']
+    search_fields = ['title','excerpt','body']
+    prepopulated_fields = {'slug': ('title',)}
